9bd22fc305dff5f03dc8f1005f4b66431249325c
Matheus Grigoleto <marthexmit@gmail.com>
2025-08-19 08:40:33 -0300

    Created Filter Status and Get route params

--- src/database/database.js
+++ src/database/database.js
@@ -6,15 +6,38 @@ export class Database {
   #database = {}
 
   constructor() {
-    fs.readFile(DATABASE_PATH, "utf8").then((data) => { 
+    fs.readFile(DATABASE_PATH, "utf8").then((data) => {
       this.#database = JSON.parse(data)
     }).catch(() => {
       this.#persist()
     })
   }
 
-  #persist(){
+  #persist() {
     fs.writeFile(DATABASE_PATH, JSON.stringify(this.#database))
+  }
+
+  insert(table, data) {
+    if (Array.isArray(this.#database[table])) {
+      this.#database[table].push(data)
+    } else {
+      this.#database[table] = [data]
+    }
+
+    this.#persist()
+  }
+
+  select(table, filters) {
+    let data = this.#database[table] ?? []
+
+    if (filters) {
+      data = data.filter((row) => {
+        return Object.entries(filters).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase())
+        })
+      })
+    }
+    return data
   }
 
 }
